c2a90306c35f9a3350a7ca8d068f6ccb0f95faf6
Nikunj <nikunj.singh@live.com>
2025-03-24 23:13:18 -0400

    responsive  navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
-function Navbar () {
-    return (
-       <div>Hello</div>
-    )
+function Navbar() {
+  const [navbar, setNavbar] = useState(false);
+
+  const handleNavbar = () => {
+    setNavbar(!navbar);
+  };
+
+  return (
+    <div className="bg-black text-gray-400 h-[100px] max-w-[1200px] mx-auto flex justify-between items-center">
+      <h1 className="text-3xl font-bold ml-4">Nikunj Singh</h1>
+      <ul className="hidden md:flex">
+        <li className="p-5">About</li>
+        <li className="p-5">Contact</li>
+      </ul>
+
+      <div onClick={handleNavbar} className="block md:hidden mr-6">
+        {navbar ? <X size={25} color="#ffffff" /> : <Menu size={25} color="#ffffff"/>}
+      </div>
+
+      <div
+        className={
+          navbar
+            ? "fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-300"
+            : "fixed left-[-100%]"
+        }
+      >
+        <h1 className="text-3xl primary-color m-4">Nikunj Singh</h1>
+        <ul className="text-2xl p-8">
+          <li className="p-3">About</li>
+          <li className="p-3">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
