c373ed52c7cd02611acdd2337862c94480305f47
Nikunj <nikunj.singh@live.com>
2025-03-25 20:11:22 -0400

    added navbar elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,16 @@ function Navbar() {
       <h1 className="text-3xl font-bold ml-4 primary-color">Nikunj Singh</h1>
       <ul className="hidden md:flex">
         <li className="p-5">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p-5">
           <a href="#about">About</a>
+        </li>
+        <li className="p-5">
+          <a href="#skills">Skills</a>
+        </li>
+        <li className="p-5">
+          <a href="#experience">Experience</a>
         </li>
         <li className="p-5">
           <a href="#contact">Contact</a>
@@ -31,18 +40,27 @@ function Navbar() {
       <div
         className={
           navbar
-            ? "fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-300"
+            ? "fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-300 z-10"  
             : "fixed left-[-100%]"
         }
       >
         <h1 className="text-3xl primary-color m-4">Nikunj Singh</h1>
         <ul className="text-2xl p-8">
-          <li className="p-3">
-            <a href="#about">About</a>
-          </li>
-          <li className="p-3">
-            <a href="#contact">Contact</a>
-          </li>
+        <li className="p-3">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p-3">
+          <a href="#about">About</a>
+        </li>
+        <li className="p-3">
+          <a href="#skills">Skills</a>
+        </li>
+        <li className="p-3">
+          <a href="#experience">Experience</a>
+        </li>
+        <li className="p-3">
+          <a href="#contact">Contact</a>
+        </li>
         </ul>
       </div>
     </div>
